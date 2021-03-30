58efdb513ff13bddc4bfcc5007cfb30373fb74a4
rosstimo <rosstimo@isu.edu>
2021-03-30 08:51:33 -0600

    Add project files.

---
+++ FileIOExample/FileIOForm.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FileIOForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GoButton = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.OpenFileDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.SuspendLayout()
+        '
+        'GoButton
+        '
+        Me.GoButton.Location = New System.Drawing.Point(63, 173)
+        Me.GoButton.Name = "GoButton"
+        Me.GoButton.Size = New System.Drawing.Size(188, 80)
+        Me.GoButton.TabIndex = 0
+        Me.GoButton.Text = "&Go"
+        Me.GoButton.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(64, 49)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(307, 26)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(64, 81)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(307, 26)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(64, 113)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(307, 26)
+        Me.TextBox3.TabIndex = 3
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(258, 173)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(188, 80)
+        Me.ExitButton.TabIndex = 4
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog
+        '
+        Me.OpenFileDialog.FileName = "OpenFileDialog1"
+        '
+        'FileIOForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(472, 283)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.GoButton)
+        Me.Name = "FileIOForm"
+        Me.Text = "Files"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GoButton As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents OpenFileDialog As OpenFileDialog
+End Class
---
+++ FileIOExample/FileIOForm.vb
@@ -0,0 +1,53 @@
+﻿Public Class FileIOForm
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        'Me.Close()
+        ReadFile()
+    End Sub
+
+    Private Sub GoButton_Click(sender As Object, e As EventArgs) Handles GoButton.Click
+        'TODO
+        WriteToFile()
+    End Sub
+
+    Sub WriteToFile()
+
+        Dim fileNumber As Integer = FreeFile()
+        Dim fileName As String = "data.txt"
+
+        FileOpen(fileNumber, fileName, OpenMode.Append)
+
+        Write(fileNumber, TextBox1.Text)
+        Write(fileNumber, TextBox2.Text)
+        WriteLine(fileNumber, TextBox3.Text)
+
+        FileClose(fileNumber)
+
+    End Sub
+
+    Sub ClearFields()
+        'TODO
+    End Sub
+
+    Sub ReadFile()
+        Dim fileNumber As Integer = FreeFile()
+        Dim fileName As String = "data.txt"
+        Dim currentRecord As String
+
+        Try
+            FileOpen(fileNumber, fileName, OpenMode.Input)
+            Do Until EOF(fileNumber)
+                Input(fileNumber, currentRecord)
+                Console.WriteLine(currentRecord)
+                Console.ReadLine()
+            Loop
+        Catch ex As Exception
+            'TODO user pick file
+            OpenFileDialog.ShowDialog()
+            fileName = OpenFileDialog.FileName
+        End Try
+
+        FileClose(fileNumber)
+
+    End Sub
+
+End Class
